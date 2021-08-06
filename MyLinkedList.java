13dde577c4c9cbe9f7685ec9f36ff0502c5a196a
KratiGupta <kratigupta8545@gmail.com>
2021-08-06 13:51:05 +0530

    Added 30,56 to 70 in linked list

---
+++ MyLinkedList.java
@@ -0,0 +1,54 @@
+package linkedlist;
+
+public class MyLinkedList {
+	
+	
+		public INode head;
+		public INode tail;
+		
+		public MyLinkedList() {
+			this.head = null;
+			this.tail = null;
+			}
+
+		public void add(INode newNode) {
+			// TODO Auto-generated method stub
+			if(this.tail == null) {
+				this.tail = newNode;
+			}
+			if(this.head == null) {
+				this.head = newNode;
+			}else {
+				INode tempNode = this.head;
+				this.head = newNode;
+				this.head.setNext(tempNode);
+			}
+		}
+		
+		public void printNodes(){
+			StringBuffer myNodes = new StringBuffer("My Nodes : ");
+			INode tempNode = head;
+			while(tempNode.getNext() != null) {
+				myNodes.append(tempNode.getKey());
+				if(!tempNode.equals(tail)) myNodes.append("->");
+				tempNode = tempNode.getNext();
+			}
+			myNodes.append(tempNode.getKey());
+			System.out.println(myNodes);
+		}
+
+		public void append(INode newNode) {
+			// TODO Auto-generated method stub
+			if(this.head == null) {
+				this.head = newNode;
+			}
+			if(this.tail == null) {
+				this.tail = newNode;
+			}else {
+				this.tail.setNext(newNode);
+				this.tail = newNode;
+			}
+		}
+	}
+
+

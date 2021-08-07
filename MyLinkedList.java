4f669d64c34a025fae60daae85f846c12553f42a
KratiGupta <kratigupta8545@gmail.com>
2021-08-07 12:08:43 +0530

    Added search feature

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,6 @@
 package linkedlist;
 
-public class MyLinkedList {
+public class MyLinkedList<K> {
 	
 	
 		public INode head;
@@ -51,10 +51,10 @@ public class MyLinkedList {
 		}
 		
 		
-		public void insert(INode mynode , INode newNode) {
+		public void insert(INode myNode , INode newNode) {
 			
-			INode tempNode = mynode.getNext();
-			mynode.setNext(newNode);
+			INode tempNode = myNode.getNext();
+			myNode.setNext(newNode);
 			newNode.setNext(tempNode);
 	}
 
@@ -75,5 +75,77 @@ public class MyLinkedList {
 			tempNode = tempNode.getNext();
 			return tempNode;
 		}
+		
+		   class Node{  
+		        int data;  
+		        Node next;  
+		          
+		        public Node(int data) {  
+		            this.data = data;  
+		            this.next = null;  
+		        }  
+		    }  
+		   
+		public  INode search(K key) {
+			 
+		        int i = 1;  
+		        boolean flag = false;  
+		       
+			
+			INode tempNode = head;
+			while(tempNode != null && tempNode.getNext()  != null) {
+				
+			     Object data = 30;
+				if(tempNode.getKey() == data) {  
+	                    flag = true;  
+	                    break;  
+	                }  
+	                i++;  
+	                tempNode = tempNode.getNext();  
+	            }  
+	          
+	        if(flag)  
+	             System.out.println("Element is present in the list at the position : " + i);
+			return tempNode;  
+	         
+			}
+			
+	
+		
+			/*public INode InsertPos(INode myNode, int position, int data, INode newNode1) {
+				data = 40;
+				INode tempNode = head;
+				if (position < 2)
+					System.out.print("Invalid position");
+
+				// if position is 1 then new node is
+				// set infornt of head node
+				// head node is changing.
+				if (position == 1) {
+					INode newNode1 = 40;
+					newNode1.nextNode = headNode;
+					head = newNode1;
+				} else {
+					while (position-- != 0) {
+						if (position == 1) {
+							// adding Node at required position
+							Node newNode = GetNode(data);
+
+							// Making the new Node to point to
+							// the old Node at the same position
+							newNode.nextNode = headNode.nextNode;
+
+							// Replacing current with new Node
+							// to the old Node to point to the new Node
+							headNode.nextNode = newNode;
+							break;
+						}
+						headNode = headNode.nextNode;
+					}
+					if (position != 1)
+						System.out.print("Position out of range");
+				}
+				return head;
+			} */
 }
 
---
+++ SearchTest.java
@@ -0,0 +1,28 @@
+package linkedlist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchTest {
+
+	@Test
+	public void test() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.search(mySecondNode);
+		
+		
+		boolean result = myLinkedList.head.equals(myFirstNode) &&
+		         myLinkedList.head.getNext().equals(mySecondNode) &&
+		         myLinkedList.tail.equals(myThirdNode) && myLinkedList.search(myLinkedList).equals(mySecondNode);
+				Assert.assertTrue(result);
+	}
+
+}

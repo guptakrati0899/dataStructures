e063cda71373f74ae719edec7db3b2ee57d01d1a
KratiGupta <kratigupta8545@gmail.com>
2021-08-07 12:45:21 +0530

    Refactored MYNodeTest

--- MyNodeTest.java
+++ MyNodeTest.java
@@ -12,7 +12,7 @@ public class MyNodeTest {
 
 			MyNode<Integer> myFirstNode = new MyNode<>(56);
 			MyNode<Integer> mySecondNode = new MyNode<>(30);
-			MyNode<Integer> myThirdNode = new MyNode<>(56);
+			MyNode<Integer> myThirdNode = new MyNode<>(70);
 			
 			myFirstNode.setNext(mySecondNode);
 			mySecondNode.setNext(myThirdNode);

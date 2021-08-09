9cbde7873fce1479c83825e52281ba3e5e7ed212
KratiGupta <kratigupta8545@gmail.com>
2021-08-09 16:24:24 +0530

    Refactored the linked list search method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -86,34 +86,95 @@ public class MyLinkedList<K> {
 		        }  
 		    }  
 		   
-		public  INode search(K key) {
-			 
+		   public  INode search(K key) { 
 		        int i = 1;  
 		        boolean flag = false;  
-		       
-			
+		      
 			INode tempNode = head;
 			while(tempNode != null && tempNode.getNext()  != null) {
 				
-			     Object data = 30;
-				if(tempNode.getKey() == data) {  
-	                    flag = true;  
-	                    break;  
-	                }  
-	                i++;  
-	                tempNode = tempNode.getNext();  
-	            }  
-	          
-	        if(flag)  
-	             System.out.println("Element is present in the list at the position : " + i);
-			return tempNode;  
-	         
+				if(tempNode.getKey().equals(key)) {  
+		                flag = true;  
+		                break;  
+		        }  
+		        i++;  
+		        tempNode = tempNode.getNext();  
+		    }   
+		    if(flag) {
+		    	System.out.println("Element is present in the list at the position : " + i);
+		    	return tempNode;
 			}
-			
+		    return null;
+			}
 		
-			
+		
+		
+
+		public void popEle(K key) {
+			INode<K> tempNode = this.head;
+			INode<K> temp;
+			while(tempNode!=null) {
+				temp = tempNode;
+				if(tempNode.getNext().getKey() == key) {
+					temp.setNext(tempNode.getNext().getNext());
+					tempNode.getNext().setNext(null);
+					System.out.println("Element deleted: " + key);
+					break;
+				}
+				tempNode=tempNode.getNext();
+				if(tempNode.getNext()==null && tempNode.getKey()!= key)
+				{
+					System.out.println("Element "+key+" not found");
+					break;
+				}
+			}	
 		}
 		
+		public void delete(K key) {
+			INode<K> tempNode = this.head;
+			INode<K> tempNode1 = this.head;
+			int i=1,length=0;
+			while(tempNode!=null) {
+				if(tempNode.getKey() == key) {
+					break;
+				}
+				i++;
+				tempNode=tempNode.getNext();
+				if(tempNode.getNext()==null && tempNode.getKey()!= key)
+				{
+					System.out.println("Element "+key+" not found");
+					break;
+				}
+			}
+			while(tempNode1!=null) {
+				length++;
+				tempNode1=tempNode1.getNext();
+			}
+			if(i==1){
+				pop();
+			}
+			else if(i==length) {
+				popLast();
+			}
+			else {
+				popEle(key);
+			}
+		}
+
+		@Override
+		public String toString() {
+			 return "MyLinkedListNodes{" + head + '}';
+		}
+
+		
+		
+
+
+		
+			
+}
+		
+
 			
 	
 		
